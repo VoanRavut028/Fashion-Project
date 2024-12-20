a5e3d25dbdaa9447738a98c32e5396f6613bdaa4
RavutDeveloper <ravut5687@gmail.com>
2024-12-20 10:12:07 +0700

    Navbar

---
+++ Script/mobile.js
@@ -0,0 +1,8 @@
+function openNavbar() {
+  document.getElementById("navbar").style.left = "0";
+  document.getElementById("navbar").style.width = "80%";
+}
+
+function closeNavbar() {
+  document.getElementById("navbar").style.left = "-100%";
+}
